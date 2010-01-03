d3f5a9e2bdbd7861385a52848913aea355026bd4
unknown <Brian@.(none)>
2010-01-02 23:05:08 -0500

    Initial version of Don't Be Late

---
+++ DontBeLate/SettingsDialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace RiotousLabs.DontBeLate
+{
+    public partial class SettingsDialog : Form
+    {
+        public SettingsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DontBeLate/SettingsDialog.designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RiotousLabs.DontBeLate
+{
+    partial class SettingsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.EnabledCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email Address:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(48, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(129, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "&Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.EmailTextBox.Location = new System.Drawing.Point(15, 26);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(189, 20);
+            this.EmailTextBox.TabIndex = 1;
+            // 
+            // EnabledCheckBox
+            // 
+            this.EnabledCheckBox.AutoSize = true;
+            this.EnabledCheckBox.Location = new System.Drawing.Point(15, 53);
+            this.EnabledCheckBox.Name = "EnabledCheckBox";
+            this.EnabledCheckBox.Size = new System.Drawing.Size(132, 17);
+            this.EnabledCheckBox.TabIndex = 2;
+            this.EnabledCheckBox.Text = "Send Reminder Emails";
+            this.EnabledCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SettingsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 116);
+            this.ControlBox = false;
+            this.Controls.Add(this.EnabledCheckBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "SettingsDialog";
+            this.Text = "Don\'t Be Late Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.TextBox EmailTextBox;
+        internal System.Windows.Forms.CheckBox EnabledCheckBox;
+    }
+}
